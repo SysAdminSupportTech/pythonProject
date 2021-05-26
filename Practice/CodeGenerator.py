477712b0fac0039966469211bbf61c2af39f8186
alberteromosele0 <alberteromosele0@gmail.com>
2021-05-26 23:20:48 +0100

    Completed the first project called Codegenerator.py that access users input and generate a code

--- Practice/CodeGenerator.py
+++ Practice/CodeGenerator.py
@@ -1,6 +1,11 @@
+import os
 hiddenMessage = "Your encryption password is: DONE05324"
 usernameMessage = input("Enter Your Username to verift who you are: ")
 print("Welcome Mr : {}".format(usernameMessage))
+
+#Add User Name to a file
+username = open("username.txt","a") #Write code to a text editor
+username.write("\n{}".format(usernameMessage))
 
 userinput1 = input("why are you here: ")
 if userinput1 == 'code':
