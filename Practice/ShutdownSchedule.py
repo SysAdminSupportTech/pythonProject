60bad0d1731e7acb2483cbf95822a17830845512
alberteromosele0 <alberteromosele0@gmail.com>
2021-09-15 22:15:13 +0100

    Program shutdownschedule.py to shutdown computer in hours

--- Practice/ShutdownSchedule.py
+++ Practice/ShutdownSchedule.py
@@ -6,75 +6,24 @@ window = Tk() #initializing the window class
 window.title("Shutdown Scheduler")
 window.geometry("350x230")
 label_top = Label(bg="white",fg="white", width= 100, height=15).pack()
-#Creating a dropdown list
-schedule_time_morning = [
-    "Morning",
-    "00:00 AM",
-    "01:00 AM",
-    "01:30 AM",
-    "02:00 AM",
-    "02:30 AM",
-    "03:00 AM",
-    "03:30 AM",
-    "04:00 AM",
-    "05:00 AM",
-    "05:30 AM",
-    "06:00 AM",
-    "06:30 AM",
-    "07:00 AM",
-    "07:30 AM",
-    "08:00 AM",
-    "08:30 AM",
-    "09:00 AM",
-    "09:30 AM",
-    "10:00 AM",
-    "10:30 AM",
-    "11:00 AM",
-    "11:30 AM",
-
-    "Afternoon",
-    "12:00 PM",
-    "12:30 PM",
-    "01:00 PM",
-    "01:30 PM",
-    "02:00 PM",
-    "02:30 PM",
-    "03:00 PM",
-    "03:30 PM",
-    "04:00 PM",
-    "04:30 PM",
-    "05:00 PM",
-    "05:30 PM",
-    "06:00 PM",
-    "06:30 PM",
-    "07:00 PM",
-    "07:30 PM",
-    "08:00 PM",
-    "08:30 PM",
-    "09:00 PM",
-    "10:30 PM",
-    "11:00 PM",
-    "11:30 PM",
-    "12:00 PM",
-    "12:30 PM"
-]
-
+#time Text entry value
+time_label = Label(text="Enter Time in Hours")
+time_label.place(x=100, y=5)
+time_entry = Entry(window)
 #Getting the value of morning
 def schedule():
     """
         Use powershell to schedule the shutdown
     """
-    
-    timeval = ''
-    os.system(shutdown /s /t)
-    #selected = clicked_morning.get()
-    #print(selected)
+    selected = time_entry.get()
+    time_call = 60*60*int(selected)
+    timer= str(time_call)
+    os.system('shutdown /s /t ' + timer)
 
-clicked_morning = StringVar()
-clicked_morning.set(schedule_time_morning[0])
-dropdown_morning = OptionMenu(window,clicked_morning, *schedule_time_morning)
-dropdown_morning.place(x=50, y=10, width= 250)
+def btn_cancel():
+    pass
 
+time_entry.place(x=50, y=30, width= 250, height=25)
 #Creating Check button strict and Mild
 var = IntVar()
 strict = Radiobutton(window, text="STRICT", variable=var, value=1)
---
+++ Practice/slizing.py
@@ -0,0 +1,5 @@
+userval = '5:02:00'
+s1 = slice(userval[0])
+s2 = slice(userval[:3])
+print(s1)
+print(s2)
