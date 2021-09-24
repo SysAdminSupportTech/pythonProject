a655183019b390f39f1879ab8870fbed301663cb
alberteromosele0 <alberteromosele0@gmail.com>
2021-09-24 22:35:11 +0100

    Close Button Completed

--- project/ShutdownSchedule.py
+++ project/ShutdownSchedule.py
@@ -1,15 +1,17 @@
 from tkinter import *
 import os
 import time
+import wmi
 
 window = Tk() #initializing the window class
-window.title("Shutdown Scheduler")
-window.geometry("350x230")
-label_top = Label(bg="white",fg="white", width= 100, height=15).pack()
+window.title("Shutdown Scheduler") #Display the title of my window
+window.geometry("350x230") #Set the size of my window
+label_top = Label(bg="white",fg="white", width= 100, height=15).pack() #Arranging the content of my windows
 #time Text entry value
 time_label = Label(text="Enter Time: h for hrs: m for min(5h or 5m)")
 time_label.place(x=60, y=5)
 time_entry = Entry(window)
+
 #Getting the value of morning
 def schedule():
     """
@@ -36,6 +38,14 @@ def schedule():
         os.system('shutdown /s /t ' + timer)
 
 def btn_cancel():
+    conn = wmi.WMI() #Establish wmi connection
+    for proc in conn.win32_Process(name="python.exe"): #loop through my process and search for python process
+        cid = os.getpid() # Get my current python process
+        if proc.ProcessId == cid: #Check against the python process i got
+            proc.terminate() #Call wmi terminate function to kill application
+
+
+def reschedule_button():
     pass
 
 time_entry.place(x=50, y=30, width= 250, height=25)
@@ -49,7 +59,7 @@ mild.place(x=180, y=80)
 #Creating the schedule button
 button_submit = Button(text="Schedule", background="green", fg="white",font=12, command=schedule).place(x=12, y=150)
 button_reschedule = Button(text="Reschedule", background="grey", fg="white", font=12).place(x=120, y=150)
-button_cancel = Button(text="Cancel", bg="red",fg="black", font=12).place(x=250, y=150)
+close_cancel = Button(text="Close", bg="red",fg="black", font=12, command=btn_cancel).place(x=250, y=150)
 
 
 label = Label(window)
