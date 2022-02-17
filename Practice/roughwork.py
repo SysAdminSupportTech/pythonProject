14fa6a4fd1e05904b4172180be0089c6dd60d41a
alberteromosele0 <alberteromosele0@gmail.com>
2022-02-17 19:04:28 +0100

    PRACTICE: Working with Psutil module

--- Practice/roughwork.py
+++ Practice/roughwork.py
@@ -1,19 +1,3 @@
-from cgi import test
-
-
-empty_tuple = ()
-print(type(empty_tuple))
-
-test1 = 1,2,3,4
-test2 = 1,
-
-print(test1)
-print(test2)
-
-print("Working With Turple Unpacking")
-survey = (21, "Switzerland", 'False')
-age, country, knows_python = survey
-
-print("Age =", age)
-print("Country =", country)
-print("Know_Pyton =", knows_python)
+import psutil as psu
+io_counter = psu.disk_usage("/")
+print(io_counter)
