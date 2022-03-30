0fc1e0d6a7c9ecf783acdf33599ec1568674c10b
Albert Eromosele <alberteromosele0@gmail.com>
2022-03-31 00:06:23 +0100

    Working on the Percent Display

--- project/monitorResouce/monitorResource.py
+++ project/monitorResouce/monitorResource.py
@@ -1,7 +1,11 @@
 #Get CPU Utilization
+from cgitb import text
+from re import X
+from turtle import bgcolor, color
 import psutil as psu
 import time
 import threading # this allows me to run while loops in parrallel
+import tkinter as tk
 
 def cpu_utilization():
     """
@@ -32,3 +36,33 @@ def disk_utilization():
 
 #thread1 = threading.Thread(target=cpu_utilization)
 #thread1.start()
+
+#Working With the TKinter GUI
+RProgram = tk.Tk()
+RProgram.geometry("300x300")
+RProgram.title("Resource Monitor")
+RProgram.resizable(False,False)
+cbox = tk.Canvas(RProgram, bg="black", height=300, width=300) #Declaring the canvas
+title_nav = tk.Frame(RProgram, bg="white", height=15).pack() #title frame
+title_label = tk.Label(title_nav, text="Resource Window Monitor", background="black",foreground="white", width=300).pack()
+CPU_Frame = tk.Frame(RProgram).pack() #CPU Frame
+CPU_Label = tk.Label(CPU_Frame, text="CPU_UTILIZATION").place(x="6", y="43",height=40, width=120)
+
+CPU_entry = tk.Entry(RProgram).place(x="130", y="43",height=40, width=164)
+
+disk_Frame = tk.Frame(RProgram).pack()
+disk_Label = tk.Label(disk_Frame, text="Disk_UTILIZATION").place(x="6", y="92",height=40, width=120)
+
+RAM_Frame = tk.Frame(RProgram).pack()
+RAM_Label = tk.Label(disk_Frame, text="RAM_UTILIZATION").place(x="6", y="145",height=40, width=120)
+
+other_proc = tk.Frame(RProgram, background="black", bg="white").place(x=6, y=200, height=90, width=286)
+high_proc_label =tk.Label(other_proc, text="High Processes").place(x=6, y=200, height=90, width=120)
+
+pro_label = tk.Label(other_proc, text="Process Name").place(x=125, y=200, width=120)
+pro_label2 = tk.Label(other_proc, text=" %").place(x=238, y=200, width=50)
+
+
+
+cbox.pack() #Canvas frame closed
+RProgram.mainloop() #Initialize program
